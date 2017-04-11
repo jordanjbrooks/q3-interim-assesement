715ed27d7f10ea2a145ba217f48f116c7ee965e3
Jordan Brooks <jbrooks@wlapcs.org>
2017-04-11 17:09:12 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var c=139;
+var j=250;
+
+function setup(){ createCanvas(600,400);}
+
+function draw(){
+if (mouseX > 300)
+{background(206, 26, 35); }
+else{background (37, 13, 145);}
+line(30,95,48,34);
+ellipse(c,j,50,200);
+ellipse(300,200,50,50 );
+rect(45,200,64,100);
+}
+
+var habits= [ "jordan","alexis","ricco","kita","carter","barbis","josselyn"]
+alert(habits[1]);
+alert(habits[4]);
+alert(habits[7]);
+
+
+
+
+
